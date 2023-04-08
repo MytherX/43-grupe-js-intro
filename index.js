fe2da4366d7c7175804938a1b0d89ba05f570b5b
MytherX~ <arnoldasbelozaravicius@gmail.com>
2023-04-08 17:10:39 +0300

    String

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-console.log(42);     //Komentaras salia
+/*
+GRAND PLANAS:
+- kintamieji / atminties ląstelės (juose atsimenama informacija)
+- if'as (palyginimas)
+- for'as (ciklai)
+- funkcijos (function)
+- įvykiai (event listeners)
+*/
+
+
+// Vienos eilutes komentaras
+
+// Multi
+// line
+// komentaras
+
+/*
+Multi
+line
+komentaras
+*/
+
+console.log(42);    // Komentaras salia
 console.log(41);
-console.log(40)
+console.log(40);
+
+// extra
+
+console.log(39);
+console.log(38);
+console.log(37);
